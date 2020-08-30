3a1d410f67bf93f21bbb5cc5ae6b30b2730bb798
JJ1SLR <JJ1SLR@jarl.com>
2020-08-30 17:15:59 +0900

    Just format the code.

--- MorseListening.ino
+++ MorseListening.ino
@@ -1,25 +1,21 @@
 #include <LiquidCrystal.h>
 #include <IRremote.h>
 
-//#define DEBUG
-//#define DEBUG_ERR
 
-#ifdef DEBUG
-#define DBG_MSG(msg) Serial.print(F("DBG: ")); Serial.println(msg);
-#define DBG_MSG_FMT(msg, fmt) Serial.print(F("DBG: ")); Serial.println(msg, fmt)
-#else
-#define DBG_MSG(msg)
-#define DBG_MSG_FMT(msg, fmt)
-#endif
+//******************************************************************************
+// Hardware Defination
+//******************************************************************************
+#define BUZZER_PIN 9
+#define LED_PIN 8
 
-#ifdef DEBUG_ERR
-#define DBG_ERR(msg) Serial.print(F("ERR: ")); Serial.print(msg)
-#define DBG_ERR_FMT(msg, fmt) Serial.print(F("ERR: ")); Serial.print(msg, fmt)
-#else
-#define DBG_ERR(msg)
-#define DBG_ERR_FMT(msg, fmt)
-#endif
+// LCD defination
+LiquidCrystal g_lcd(12, 11, 7, 6, 5, 4);
+#define LCD_COLS 16
+#define LCD_ROWS 2
 
+// Remote control global variables
+volatile IRrecv g_irrecv(2);
+volatile decode_results g_results;
 
 // Remote key defination
 #define KEY_CH_MINUS 0xFFA25D
@@ -51,19 +47,43 @@
 #define KEY_9 0xFF52AD
 
 #define KEY_LAST 0xFFFFFFFF
+//------------------------------------------------------------------------------
 
-// Macro functions
-#define ARY_LEN(ary) (sizeof(ary)/sizeof(ary[0]))
 
-// Pins defination
-#define BUZZER_PIN 9
-#define REMOTE_PIN 2
-#define LED_PIN 8
-
-// CONST STRINGS
+//******************************************************************************
+// Software configure Defination
+//******************************************************************************
 #define CQ_CALL_SIGN "CQ CQ CQ DE JJ1SLR JJ1SLR K"
+//------------------------------------------------------------------------------
 
+
+//******************************************************************************
+// DEBUG Macros
+//******************************************************************************
+//#define DEBUG
+//#define DEBUG_ERR
+
+#ifdef DEBUG
+#define DBG_MSG(msg) Serial.print(F("DBG: ")); Serial.println(msg);
+#define DBG_MSG_FMT(msg, fmt) Serial.print(F("DBG: ")); Serial.println(msg, fmt)
+#else
+#define DBG_MSG(msg)
+#define DBG_MSG_FMT(msg, fmt)
+#endif
+
+#ifdef DEBUG_ERR
+#define DBG_ERR(msg) Serial.print(F("ERR: ")); Serial.print(msg)
+#define DBG_ERR_FMT(msg, fmt) Serial.print(F("ERR: ")); Serial.print(msg, fmt)
+#else
+#define DBG_ERR(msg)
+#define DBG_ERR_FMT(msg, fmt)
+#endif
+//------------------------------------------------------------------------------
+
+
+//******************************************************************************
 // Morse data structure
+//******************************************************************************
 // Offset
 #define MTABLE_CHAR_OFFSET 0x20
 // Morse storage structure
@@ -133,19 +153,31 @@ const MorseCode mTable[] = {
   { 4, B10110000 },       // 0x59, Y | －・－－
   { 4, B11000000 },       // 0x5A, Z | －－・・
 };
+//------------------------------------------------------------------------------
 
-// Lcd defination
-LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
 
+//******************************************************************************
+// Macro functions
+//******************************************************************************
+#define ARY_LEN(ary) (sizeof(ary)/sizeof(ary[0]))
+//------------------------------------------------------------------------------
+
+
+//******************************************************************************
+// Global variables
+//******************************************************************************
 // sound last time ( = one dot time)
-volatile unsigned long g_duration = 80;
+#define DEFAULT_DURATION 80
+volatile unsigned long g_duration = DEFAULT_DURATION;
 // sound g_frequency
-volatile unsigned int g_frequency = 1000;
+#define DEFAULT_FREQUENCY 1000
+volatile unsigned int g_frequency = DEFAULT_FREQUENCY;
+//------------------------------------------------------------------------------
 
-// Remote control global variables
-volatile IRrecv g_irrecv(REMOTE_PIN);
-volatile decode_results g_results;
 
+//******************************************************************************
+// Basic functions
+//******************************************************************************
 void playTone(int freq, int dura) {
   digitalWrite(LED_PIN, HIGH);
   tone(BUZZER_PIN, freq, dura);
@@ -174,7 +206,12 @@ void playWordSep() {
 bool getBit(byte dat, byte i) {
   return (bool)((0x01 << (7 - i)) & dat);
 }
+//------------------------------------------------------------------------------
 
+
+//******************************************************************************
+// Module functions
+//******************************************************************************
 bool playChar(char ch, bool disp) {
 //  Serial.println(ch);
   byte ic;  // index of mTable
@@ -201,21 +238,21 @@ bool playChar(char ch, bool disp) {
     return false;
   }
 
-  // display char on LCD
+  // display char on g_lcd
   if (disp) {
     // display char on row 0
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print(F("Charactor: "));
-    lcd.write(ch);
+    g_lcd.clear();
+    g_lcd.setCursor(0, 0);
+    g_lcd.print(F("Charactor: "));
+    g_lcd.write(ch);
     // display Morse on row 1
-    lcd.setCursor(0, 1);
-    lcd.print(F("Morse: "));
+    g_lcd.setCursor(0, 1);
+    g_lcd.print(F("Morse: "));
     for (int i = 0; i < mTable[ic].len; ++i) {
       if (getBit(mTable[ic].dat, i)) {
-        lcd.write('-');
+        g_lcd.write('-');
       } else {
-        lcd.write('.');
+        g_lcd.write('.');
       }
     }
   }
@@ -237,19 +274,24 @@ bool playChar(char ch) {
 }
 
 void playCQCallSign() {
-  lcd.cursor();
-  lcd.blink();
+  g_lcd.cursor();
+  g_lcd.blink();
   char *pc = CQ_CALL_SIGN;
   byte len = strlen(pc);
   for (byte i = 0; i < len; ++i) {
     playChar(pc[i]);
-    lcd.write(pc[i]);
-    if (i == 15) {
-      lcd.setCursor(0, 1);
+    g_lcd.write(pc[i]);
+    if (i == LCD_COLS - 1) {
+      g_lcd.setCursor(0, 1);
     }
   }
 }
+//------------------------------------------------------------------------------
 
+
+//******************************************************************************
+// Public functions
+//******************************************************************************
 void ir0_handler() {
   DBG_MSG(F("ir0_handler: Enter Interrupt"));
   if (g_irrecv.decode(&g_results)) {
@@ -267,10 +309,10 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);
   // initialize buzzer output pin
   pinMode(BUZZER_PIN, OUTPUT);
-  // initialize lcd
-  lcd.begin(16, 2);
-  lcd.cursor();
-  lcd.blink();
+  // initialize g_lcd
+  g_lcd.begin(LCD_COLS, LCD_ROWS);
+  g_lcd.cursor();
+  g_lcd.blink();
   // Start the receiver
   g_irrecv.blink13(true);
   g_irrecv.enableIRIn(); 
@@ -278,10 +320,9 @@ void setup() {
 }
 
 void loop() {
-
   // playCQCallSign();
-  lcd.noCursor();
-  lcd.noBlink();
+  g_lcd.noCursor();
+  g_lcd.noBlink();
   for (int i = 1; i < ARY_LEN(mTable); ++i) {
     if (playChar(i + MTABLE_CHAR_OFFSET, true)) {
       delay(1000); 
@@ -290,6 +331,6 @@ void loop() {
 
   delay(5000);
  
-  lcd.clear();
-  lcd.setCursor(0, 0);
+  g_lcd.clear();
+  g_lcd.setCursor(0, 0);
 }
